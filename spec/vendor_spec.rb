0c216502ac24fd6ba3c8de755697f0b877b25dc8
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-08 09:22:34 -0600

    test: add test for the #check_stock method in the vendor spec

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -3,16 +3,22 @@ require './lib/vendor'
 
 RSpec.describe Vendor do 
   before (:each) do 
-    # @item1 = Item.new({name: 'Peach', price: "$0.75"})
-    # @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
     @vendor = Vendor.new("Rocky Mountain Fresh")
   end
 
   it "exists and has readable attributes" do 
     expect(@vendor).to be_a Vendor
-    expect(vendor.name).to eq "Rocky Mountain Fresh"
+    expect(@vendor.name).to eq "Rocky Mountain Fresh"
     
-    expect(vendor.inventory).to eq {}
-    
+    expect(@vendor.inventory).to eq({})
+    expect(@item1.name).to eq("Peach")
+  end
+
+  describe "#check_stock" do 
+    it "can read the inventory hash and return the stock of the item as an integer" do
+      expect(@vendor.check_stock(@item1)).to eq(0)
+    end
   end
 end
