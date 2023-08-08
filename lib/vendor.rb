a6fccd4a1117e42168ed229ee6d080f7f99e2d35
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-08-08 09:53:03 -0600

    feat: create #stock method

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,7 +1,16 @@
 class Vendor 
-  attr_reader :name, :inventory
+  attr_reader :name
+  attr_accessor :inventory
   def initialize(name)
     @name = name 
-    @inventory = {}
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item]
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
   end
 end
